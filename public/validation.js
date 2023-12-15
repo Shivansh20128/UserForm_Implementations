06a1e4b2c156ceefa095f15250d903d32a9bdb87
shivansh <shivansh20128@iiitd.ac.in>
2023-12-15 12:31:34 +0530

    Adding a new form

---
+++ public/validation.js
@@ -0,0 +1,92 @@
+function isValidowner_Email(owner_email) {
+  // Regular expression for a basic owner_email validation
+  const owner_emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Test the owner_email against the regex
+  return owner_emailRegex.test(owner_email);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("userForm");
+
+    const validateFormName = () => {
+        const form_nameField = document.getElementById("form_name");
+        const form_nameError = document.getElementById("form_name-error");
+
+        // Reset previous error messages
+        form_nameError.textContent = "";
+
+        // Validate Name
+        if (!form_nameField.value.trim()) {
+            form_nameError.textContent = "Form Name is required!";
+            form_nameField.style.border = "1px solid red";
+        } else {
+            form_nameField.style.border = "1px solid green";
+        }
+    };
+
+    const validateowner_Email = () => {
+        const owner_emailField = document.getElementById("owner_email");
+        const owner_emailError = document.getElementById("owner_email-error");
+
+        // Reset previous error messages
+        owner_emailError.textContent = "";
+
+        // Validate Age
+        const owner_email = owner_emailField.value.trim();
+        if(owner_email.length==0){
+            owner_emailError.textContent = "owner_Email ID is required!";
+            owner_emailField.style.border = "1px solid red";
+        }
+        else if (!isValidowner_Email(owner_email)) {
+            owner_emailError.textContent = "Please enter a valid owner_email";
+            owner_emailField.style.border = "1px solid red";
+        } else {
+            owner_emailField.style.border = "1px solid green";
+        }
+    };
+
+
+    const validateQuestions = () => {
+        const no_of_questionsField = document.getElementById("no_of_questions");
+        const no_of_questionsError = document.getElementById("no_of_questions-error");
+
+        // Reset previous error messages
+        no_of_questionsError.textContent = "";
+        const numberRegex = /^(0*[1-9]|1[0-9]|20)$/;
+        // Validate Weight
+        const no_of_questions = parseInt(no_of_questionsField.value);
+        if (!numberRegex.test(no_of_questions)) {
+            no_of_questionsError.textContent = "Please enter a valid number of employees";
+            phone_numberField.style.border = "1px solid red";
+        } else {
+            phone_numberField.style.border = "1px solid green";
+        }
+    };
+
+    // Add live validation on input
+    form.addEventListener("input", function (event) {
+        if (event.target.id === "form_name") {
+            validateFormName();
+        } else if (event.target.id === "owner_email") {
+            validateowner_Email();
+        } else if (event.target.id === "no_of_questions") {
+            validateQuestions();
+        }
+    });
+
+    // Add final validation on form submission
+    form.addEventListener("submit", function (event) {
+        validateFormName();
+        validateowner_Email();
+        validateQuestions();
+        // Check if there are any error messages
+        const errorMessages = form.querySelectorAll(".error-message");
+        for (const errorMessage of errorMessages) {
+            if (errorMessage.textContent !== "") {
+                event.preventDefault();
+                return;
+            }
+        }
+    });
+});
